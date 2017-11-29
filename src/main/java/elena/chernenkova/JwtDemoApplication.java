1a52a0e684125fb64443edcbb9bc5c9b20511d14
GhostWK <Zubkov.AS@phystech.edu>
2017-11-29 14:08:48 +0300

    comment for testing

--- src/main/java/elena/chernenkova/JwtDemoApplication.java
+++ src/main/java/elena/chernenkova/JwtDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JwtDemoApplication {
 
     public static void main(String[] args) {
+        //testing comment
         SpringApplication.run(JwtDemoApplication.class, args);
     }
 }
